f074588b862a5ae52cc20ecaba7d13e44b9098a5
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2021-12-16 13:18:16 -0500

    Update config.js

--- config.js
+++ config.js
@@ -2,35 +2,35 @@ global.DeveloperMode = 'false' //true Or false
 global.linkGC = ['https://chat.whatsapp.com/Lb4Emjih98rBiCZiZoS2eM', 'https://chat.whatsapp.com/FrluVyY5kBRIhY6yKfH4DI']
 global.Owner = {
   // use the phone number with the country code, for example Indonesia '62' America '1' etc
-  '62857139649630': { // put your number here
+  '593993684821': { // put your number here
     name: 'Owner Metro Bot/Creator games-wabot',
-    isDev: false, // if true this number will send if bot have bug and developer mode true
-    isReport: false, // if true this number will send if user use command /report or /join
-    isCreator: true // if true this number will send if user use command /owner or /creator
+    isDev: false, // si es cierto, este número se enviará si el bot tiene error y el modo de desarrollador es verdadero
+    isReport: false, // si es verdadero, este número se enviará si el usuario usa el comando / informe o / unirse
+    isCreator: true // si es verdadero, este número se enviará si el usuario usa el comando / propietario o / creador
   },
-  '6281390658325': { // put your number here
-    isDev: true, // if true this number will send if bot have bug and developer mode true
-    isReport: true, // if true this number will send if user use command /report or /join
-    isCreator: false // if true this number will send if user use command /owner or /creator
+  '6281390658325': { // pon tu numero aqui
+    isDev: true, // si es cierto, este número se enviará si el bot tiene error y el modo de desarrollador es verdadero
+    isReport: true, // si es verdadero, este número se enviará si el usuario usa el comando / informe o / unirse
+    isCreator: false // si es verdadero, este número se enviará si el usuario usa el comando / propietario o / creador
   },
-  '6282256115584': { // put your number here
+  '6282256115584': { // pon tu numero aqui
     name: 'Dave',
-    isDev: false, // if true this number will send if bot have bug and developer mode true
-    isReport: true, // if true this number will send if user use command /report or /join
-    isCreator: false // if true this number will send if user use command /owner or /creator
+    isDev: false, // si es cierto, este número se enviará si el bot tiene error y el modo de desarrollador es verdadero
+    isReport: true, // si es verdadero, este número se enviará si el usuario usa el comando / informe o / unirse
+    isCreator: false // si es verdadero, este número se enviará si el usuario usa el comando / propietario o / creador
   },
   '60199782326': {
     name: 'Adiixyz',
-    isReport: true, // if true this number will send if user use command /report or /join
+    isReport: true, // si es verdadero, este número se enviará si el usuario usa el comando / informe o / unirse
   },
   '6285713964963': {
     isReport: true
   },
-  '13479805233': {} // ignore if you don't want anything, like name dll
+  '13479805233': {} // ignora si no quieres nada, como nombre, etc.
 }
-global.mods = ['13479805233'] // Want some help?
-global.prems = [] // Premium user has unlimited limit
-global.APIs = { // API Prefix
+global.mods = ['13479805233'] //¿Quieres ayuda?
+global.prems = [] // El usuario premium tiene límite ilimitado
+global.APIs = { // Prefijo de API
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',
   xteam: 'https://api.xteam.xyz',
@@ -56,7 +56,7 @@ global.APIKeys = { // APIKey Here
 global.packname = 'wa.me/6285713964963'
 global.author = 'Metro Bot Is The Best'
 
-global.multiplier = 39 // The higher, The harder levelup
+global.multiplier = 39 // Cuanto más alto, más difícil sube de nivel
 
 let fs = require('fs')
 let chalk = require('chalk')
