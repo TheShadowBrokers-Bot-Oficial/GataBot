0ab490d1306cd93de40924a584808772a0d99a09
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2021-12-16 17:39:29 -0500

    Update _afk.js

--- plugins/_afk.js
+++ plugins/_afk.js
@@ -3,7 +3,7 @@ handler.before = m => {
   let user = global.DATABASE.data.users[m.sender]
   if (user.afk > -1) {
     m.reply(`
-Su AFK${user.afkReason ? ' Finalizó ' + user.afkReason : ''}
+Su AFK${user.afkReason ? ' Finalizó: ' + user.afkReason : ''}
 Regresó al grupo. *Después de:* ${clockString(new Date - user.afk)}
 `.trim())
     user.afk = -1
@@ -18,7 +18,8 @@ Regresó al grupo. *Después de:* ${clockString(new Date - user.afk)}
     let reason = user.afkReason || ''
     m.reply(`
 *¡No lo/a etiquetes!*
-Se encuentra en AFK (Lejos del teclado). ${reason ?\n\n ' *Razón:* ' + reason : ' *Sin razón* '}
+Se encuentra en AFK (Lejos del teclado). ${reason ?\n\n 
+            ' *Razón:* ' + reason : ' *Sin razón* '}
 Sigue ausente. *Durante:* ${clockString(new Date - afkTime)}
 `.trim())
   }
