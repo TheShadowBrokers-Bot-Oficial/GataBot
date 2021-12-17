e401b90f1e89894bc371ddc34edf046d17f6fc3b
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2021-12-16 19:12:45 -0500

    Update _afk.js

--- plugins/_afk.js
+++ plugins/_afk.js
@@ -2,10 +2,11 @@ let handler = m => m
 handler.before = m => {
   let user = global.DATABASE.data.users[m.sender]
   if (user.afk > -1) {
-    m.reply(`
-Kamu berhenti AFK${user.afkReason ? ' setelah ' + user.afkReason : ''}
-Selama ${clockString(new Date - user.afk)}
-`.trim())
+    m.reply(
+*_✳️ Dejaste de estar inactivo (AFK)${user.afkReason ? ' después de estar inactivo (AFK) por el motivo: ' + user.afkReason : ''}_*
+
+*_⌛️ Tiempo de inactividad: ${clockString(new Date - user.afk)}_*
+.trim())
     user.afk = -1
     user.afkReason = ''
   }
@@ -16,11 +17,15 @@ Selama ${clockString(new Date - user.afk)}
     let afkTime = user.afk
     if (!afkTime || afkTime < 0) continue
     let reason = user.afkReason || ''
-    m.reply(`
-Jangan tag dia!
-Dia sedang AFK ${reason ? 'dengan alasan ' + reason : 'tanpa alasan'}
-Selama ${clockString(new Date - afkTime)}
-`.trim())
+    m.reply(
+*_⚠️ No lo etiquetes!!! ⚠️_*
+
+*_✳️ El usuario que mencionas está inactivo (AFK)_*
+
+*_👉${reason ? 'Motivo de inactividad: ' + reason : 'Motivo de inactividad: El usuario no especifico un motivo'}_*
+
+*_⌛️ Tiempo transcurrido de inactividad: ${clockString(new Date - afkTime)}_*
+.trim())
   }
   return true
 }
@@ -33,3 +38,4 @@ function clockString(ms) {
   let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
   return [h, m, s].map(v => v.toString().padStart(2, 0) ).join(':')
 }
+
