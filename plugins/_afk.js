b00a7f00a3e657f8436820871840e4d9ecf16dda
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2021-12-16 19:24:31 -0500

    Update _afk.js

--- plugins/_afk.js
+++ plugins/_afk.js
@@ -2,11 +2,10 @@ let handler = m => m
 handler.before = m => {
   let user = global.DATABASE.data.users[m.sender]
   if (user.afk > -1) {
-    m.reply(
-*_✳️ Dejaste de estar inactivo (AFK)${user.afkReason ? ' después de estar inactivo (AFK) por el motivo: ' + user.afkReason : ''}_*
-
-*_⌛️ Tiempo de inactividad: ${clockString(new Date - user.afk)}_*
-.trim())
+    m.reply(`
+😸 Regresó al grupo de su AFK${user.afkReason ? ' *Finalizó:* ' + user.afkReason : ''}
+*Después de:* ${clockString(new Date - user.afk)}
+`.trim())
     user.afk = -1
     user.afkReason = ''
   }
@@ -17,15 +16,11 @@ handler.before = m => {
     let afkTime = user.afk
     if (!afkTime || afkTime < 0) continue
     let reason = user.afkReason || ''
-    m.reply(
-*_⚠️ No lo etiquetes!!! ⚠️_*
-
-*_✳️ El usuario que mencionas está inactivo (AFK)_*
-
-*_👉${reason ? 'Motivo de inactividad: ' + reason : 'Motivo de inactividad: El usuario no especifico un motivo'}_*
-
-*_⌛️ Tiempo transcurrido de inactividad: ${clockString(new Date - afkTime)}_*
-.trim())
+    m.reply(`
+*¡No lo/a etiquetes!*
+😺Se encuentra en AFK (Lejos del teclado). ${reason ? ' *Razón:* ' + reason : ' *Sin razón* '}
+Sigue ausente. *Durante:* ${clockString(new Date - afkTime)}
+`.trim())
   }
   return true
 }
@@ -38,4 +33,3 @@ function clockString(ms) {
   let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
   return [h, m, s].map(v => v.toString().padStart(2, 0) ).join(':')
 }
-
