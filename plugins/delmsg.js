619e5a956650f9f21f3509406d38b25f55b37046
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2021-12-17 17:33:45 -0500

    Update delmsg.js

--- plugins/delmsg.js
+++ plugins/delmsg.js
@@ -1,10 +1,10 @@
-et handler = async (m, { command, usedPrefix, text }) => {
+let handler = async (m, { command, usedPrefix, text }) => {
     let which = command.replace(/get/i, '')
-    if (!text) throw Gunakan *${usedPrefix}list${which}* untuk melihat list nya
+    if (!text) throw Usar *${usedPrefix}list${which}* para ver la lista
     let msgs = global.DATABASE._data.msgs
-    if (!text in msgs) throw '${text}' tidak terdaftar di list pesan
+    if (!text in msgs) throw *"${text}"* no ha sido incluido en la lista de mensajes 😐
     delete msgs[text]
-    m.reply(Berhasil menghapus pesan di list pesan dengan nama '${text}')
+    m.reply( Eliminar mensajes en la lista de mensajes con el nombre *$"{text}"*. )
 }
 handler.help = ['vn', 'msg', 'video', 'gif', 'audio', 'img', 'sticker'].map(v => 'del' + v + ' <teks>')
 handler.tags = ['database']
