8599800996b1c6d2b68b05a315a570e7a0469278
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2021-12-17 10:33:50 -0500

    Update _antitoxic.js

--- plugins/_antitoxic.js
+++ plugins/_antitoxic.js
@@ -1,6 +1,6 @@
 let handler = m => m
 
-let linkRegex = /(ton(ta|to(([ie])ng|([ie])r)?)|es?tupi?do|estu?pi?da|as?co|fu?ck|bo(bo|ba)k|babo?so|ba?bosa|sa([iueo])bl([iueo])(k|g)|g (gusa(na|no)|(bast(ardo|arda)|(mise(ria|rio)([iueo])|(car?ajo)|?|(se(xo|xy)|me?nso|?|men?sa|b l ([iueo]) (k|g)|a (n j (i n g|i r)?)s u|col(i|ay)|an?jg|b([ia])ngs([ia])?t|t([iuo])l([iuo])l)/i
+let linkRegex = /(a(su|nj(([ie])ng|([ie])r)?)|me?me?k|ko?nto?l|ba?bi|fu?ck|ta(e|i)k|bangsat|g([iueo])bl([iueo])(k|g)|g ([iueo]) b l ([iueo]) (k|g)|a (n j (i n g|i r)?)s u|col(i|ay)|an?jg|b([ia])ngs([ia])?t|t([iuo])l([iuo])l)/i
 handler.before = function (m, { user }) {
   if (m.isBaileys && m.fromMe) return true
   if (/masuk|lanjutkan|banjir|(per)?panjang/g.exec(m.text)) return true
