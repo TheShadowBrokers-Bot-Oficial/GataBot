643c808b8e14c128797eeae46c975dee3f6a32bb
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2021-12-16 22:51:42 -0500

    Update _antitoxic.js

--- plugins/_antitoxic.js
+++ plugins/_antitoxic.js
@@ -1,6 +1,6 @@
 let handler = m => m
 
-let linkRegex = /(ton|to|ta))|(mal|la|lo)/
+let linkRegex = /(ton(ta|to(([ie])ng|([ie])r)?)|es?tupi?do|estu?pi?da|as?co|fu?ck|lo(ca|co)k|baboso|sa([iueo])bl([iueo])(k|g)|g ([iueo]) b l ([iueo]) (k|g)|a (n j (i n g|i r)?)s u|col(i|ay)|an?jg|b([ia])ngs([ia])?t|t([iuo])l([iuo])l)/i
 handler.before = function (m, { user }) {
   if (m.isBaileys && m.fromMe) return true
   if (/masuk|lanjutkan|banjir|(per)?panjang/g.exec(m.text)) return true
