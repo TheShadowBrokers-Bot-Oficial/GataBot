7864d5a25122c306f28df14daccae1621623bede
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2021-12-17 19:17:58 -0500

    Update delmsg.js

--- plugins/delmsg.js
+++ plugins/delmsg.js
@@ -1,10 +1,10 @@
 let handler = async (m, { command, usedPrefix, text }) => {
     let which = command.replace(/get/i, '')
-    if (!text) throw `Uso *${usedPrefix}list${which}* para ver la lista`
+    if (!text) throw `Usar *"${usedPrefix}list${which}"* para ver la lista.`
     let msgs = global.DATABASE._data.msgs
-    if (!text in msgs) throw `'${text}' no incluido en la lista de mensajes`
+    if (!text in msgs) throw `*"${text}*" no incluido en la lista de mensajes.`
     delete msgs[text]
-    m.reply(`Eliminó con éxito el mensaje en la lista de mensajes con el nombre '${text}'`)
+    m.reply(`Eliminó con éxito el mensaje en la lista de mensajes con el nombre *"${text}"*`)
 }
 handler.help = ['vn', 'msg', 'video', 'gif', 'audio', 'img', 'sticker'].map(v => 'del' + v + ' <teks>')
 handler.tags = ['database']
