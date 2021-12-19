41ff60c457a0d035e5d3bdeb1ad8eff37d7f3591
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2021-12-19 00:20:30 -0500

    Update simsimi.js

--- plugins/simsimi.js
+++ plugins/simsimi.js
@@ -1,6 +1,6 @@
 let fetch = require('node-fetch')
 let handler = async (m, { text }) => {
-  let res = await fetch(global.API KEY('https://api.simsimi.HPe8jU_gm5APBm.uxLRUIOGTnX2RJZVK6RzgZMet.net', '/v2/', { text: encodeURIComponent(text), lc: "es" }, ''))
+  let res = await fetch(global.API KEY ('https://apikey.simsimi.HPe8jU_gm5APBm.uxLRUIOGTnX2RJZVK6RzgZMet', '/v2/', { text: encodeURIComponent(text), lc: "es" }, ''))
   let json = await res.json()
   if (json.success) m.reply(json.success)
   else throw json
