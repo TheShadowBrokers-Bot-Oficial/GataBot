ba1631327e39899c106df7bce784f3ad8cbc7cb0
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2021-12-18 20:35:40 -0500

    Update anonymous_chat.js

--- plugins/anonymous_chat.js
+++ plugins/anonymous_chat.js
@@ -1,22 +1,51 @@
-let handler = m => m
+const { MessageType } = require("@adiwajshing/baileys")
 
-handler.before = async function (m, { match }) {
-    // if (match) return !1
-    if (!m.chat.endsWith('@s.whatsapp.net')) return !0
+async function handler(m, { command }) {
+    command = command.toLowerCase()
     this.anonymous = this.anonymous ? this.anonymous : {}
-    let room = Object.values(this.anonymous).find(room => [room.a, room.b].includes(m.sender) && room.state === 'CHATTING')
-    if (room) {
-        let other = [room.a, room.b].find(user => user !== m.sender)
-        m.copyNForward(other, true, m.quoted && m.quoted.fromMe ? {
-            contextInfo: {
-                ...m.msg.contextInfo,
-                forwardingScore: 1,
-                isForwarded: true,
-                participant: other
+    switch (command) {
+        case 'next':
+        case 'leave': {
+            let room = Object.values(this.anonymous).find(room => room.check(m.sender))
+            if (!room) throw 'No estás en un chat anónimo'
+            m.reply('Ok.. ha salido exitosamente')
+            let other = room.other(m.sender)
+            if (other) this.sendMessage(other, 'El otro usuario decidio abandonar el chat', MessageType.text)
+            delete this.anonymous[room.id]
+            if (command === 'leave') break
+        }
+        case 'start': {
+            if (Object.values(this.anonymous).find(room => room.check(m.sender))) throw 'Sigues en el chat anónimo'
+            let room = Object.values(this.anonymous).find(room => room.state === 'WAITING' && !room.check(m.sender))
+            if (room) {
+                this.sendMessage(room.a, 'Se conecto exitosamente con un chat anonimo!', MessageType.text)
+                room.b = m.sender
+                room.state = 'CHATTING'
+                m.reply('Se conecto exitosamente con un chat anonimo!')
+            } else {
+                let id = + new Date
+                this.anonymous[id] = {
+                    id,
+                    a: m.sender,
+                    b: '',
+                    state: 'WAITING',
+                    check: function (who = '') {
+                        return [this.a, this.b].includes(who)
+                    },
+                    other: function (who = '') {
+                        return who === this.a ? this.b : who === this.b ? this.a : ''
+                    },
+                }
+                m.reply('En espera de que un usuario use el comando para poder vincularlos.. esto puede demorar si desea salir use #leave')
             }
-        } : {})
+            break
+        }
     }
-    return !0
 }
+handler.help = ['start', 'leave', 'next']
+handler.tags = 'anonymous'
+
+handler.command = ['start', 'leave', 'next']
+handler.private = true
 
 module.exports = handler
