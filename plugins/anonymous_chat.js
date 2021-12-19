5ff1efe638a9d62c9e497c510e5db2cfb22e8667
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2021-12-18 20:29:43 -0500

    Update anonymous_chat.js

--- plugins/anonymous_chat.js
+++ plugins/anonymous_chat.js
@@ -1,51 +1,22 @@
-const { MessageType } = require("@adiwajshing/baileys")
+let handler = m => m
 
-async function handler(m, { command }) {
-    command = command.toLowerCase()
+handler.before = async function (m, { match }) {
+    // if (match) return !1
+    if (!m.chat.endsWith('@s.whatsapp.net')) return !0
     this.anonymous = this.anonymous ? this.anonymous : {}
-    switch (command) {
-        case 'next':
-        case 'leave': {
-            let room = Object.values(this.anonymous).find(room => room.check(m.sender))
-            if (!room) throw 'No estás en un chat anónimo. 👤'
-            m.reply('Ok')
-            let other = room.other(m.sender)
-            if (other) this.sendMessage(other, 'Los socios abandonan el chat', MessageType.text)
-            delete this.anonymous[room.id]
-            if (command === 'leave') break
-        }
-        case 'start': {
-            if (Object.values(this.anonymous).find(room => room.check(m.sender))) throw 'Sigues en el chat anónimo'
-            let room = Object.values(this.anonymous).find(room => room.state === 'WAITING' && !room.check(m.sender))
-            if (room) {
-                this.sendMessage(room.a, '¡Encuentra un compañero!', MessageType.text)
-                room.b = m.sender
-                room.state = 'CHAT'
-                m.reply('¡Encuentra un compañero!!')
-            } else {
-                let id = + new Date
-                this.anonymous[id] = {
-                    id,
-                    a: m.sender,
-                    b: '',
-                    state: 'ESPERE',
-                    check: function (who = '') {
-                        return [this.a, this.b].includes(who)
-                    },
-                    other: function (who = '') {
-                        return who === this.a ? this.b : who === this.b ? this.a : ''
-                    },
-                }
-                m.reply('Esperando socios ...')
+    let room = Object.values(this.anonymous).find(room => [room.a, room.b].includes(m.sender) && room.state === 'CHATTING')
+    if (room) {
+        let other = [room.a, room.b].find(user => user !== m.sender)
+        m.copyNForward(other, true, m.quoted && m.quoted.fromMe ? {
+            contextInfo: {
+                ...m.msg.contextInfo,
+                forwardingScore: 1,
+                isForwarded: true,
+                participant: other
             }
-            break
-        }
+        } : {})
     }
+    return !0
 }
-handler.help = ['start', 'leave', 'next']
-handler.tags = 'anonymous'
-
-handler.command = ['start', 'leave', 'next']
-handler.private = true
 
 module.exports = handler
