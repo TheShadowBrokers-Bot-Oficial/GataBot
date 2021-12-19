5d219a26366462bc96012aeec69fec0ee020a1bd
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2021-12-18 21:34:29 -0500

    Update pregunta.js

--- plugins/pregunta.js
+++ plugins/pregunta.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text }) => {
   conn.reply(m.chat, `
-🐣 *Pertanyaan:* ${m.text}
+🐣 *Pertanyaan:* ${text}
 🐥 *Jawaban:* ${pickRandom(['Sí ',' Probablemente sí ',' Probablemente ',' Quizás no ',' No ',' De ninguna manera ',' Es probable... ',' No es probable ',' Es imposible ',' No es imposible ',' Siempre ',' Jamás ',' Y todavía lo preguntas Jajajaja ',' Mejor no preguntes eso... ',' Claro que sí ',' Claro que no ',' Puede ser... ',' No puede ser cierto'])}
 `.trim(), m, m.mentionedJid ? {
     contextInfo: {
