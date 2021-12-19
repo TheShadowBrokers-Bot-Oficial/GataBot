9cea22e87462bb30e9b212bcc867fd630c450fb2
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2021-12-18 23:59:39 -0500

    Update simsimi.js

--- plugins/simsimi.js
+++ plugins/simsimi.js
@@ -1,14 +1,14 @@
 let fetch = require('node-fetch')
 let handler = async (m, { text }) => {
-  if (!text) throw 'Textnya apa?'
-  let res = await fetch(global.API('bg', '/simi', { pesan: text }))
+  let res = await fetch(global.API('https://api.simsimi.net', '/v2/', { text: encodeURIComponent(text), lc: "es" }, ''))
   let json = await res.json()
-  if (json.status !== true) throw json
-  m.reply(json.result.jawab)
+  if (json.success) m.reply(json.success)
+  else throw json
 }
-handler.help = ['simi', 'simsimi', 'simih'].map(v => v + ' [teks]')
-handler.tags = ['fun']
-handler.command = /^((sim)?simi|simih)$/i
+//handler.help = ['simi', 'simsimi', 'simih'].map(v => v + ' <teks>')
+//handler.tags = ['fun']
+handler.command = /^((sim)?simi|simih|s)$/i
+handler.register = false
 
 module.exports = handler
 
