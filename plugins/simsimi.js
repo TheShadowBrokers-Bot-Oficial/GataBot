4d880023fa38407972903147c9f39514cdd5b757
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2021-12-19 00:32:37 -0500

    Update simsimi.js

--- plugins/simsimi.js
+++ plugins/simsimi.js
@@ -7,7 +7,7 @@ let handler = async (m, { text }) => {
 }
 //handler.help = ['simi', 'simsimi', 'simih'].map(v => v + ' <teks>')
 //handler.tags = ['fun']
-handler.command = /^((sim)?simi|simih|s)$/i
+handler.command = /^((sim)?simi|bot|sim)$/i
 handler.register = false
 
 module.exports = handler
