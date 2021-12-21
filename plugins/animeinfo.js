ce3911f5b83fd0864b83867d4fd0e74787d9ba2d
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2021-12-21 14:36:14 -0500

    Update animeinfo.js

--- plugins/animeinfo.js
+++ plugins/animeinfo.js
@@ -1,34 +1,20 @@
 let fetch = require('node-fetch')
-let cheerio = require('cheerio')
-var jsdom = require('jsdom')
-const { JSDOM } = jsdom
-let handler = async (m, { conn, text }) => {
-  if (!text) throw `Masukkan query!`
-  let res = await fetch(global.API('http://localhost:8000/v3/', 'search/anime', { q: text }))
+let handler = async(m, { conn, text }) => {
+  if (!text) throw Enter a query!
+  let res = await fetch(global.API('https://api.jikan.moe', '/v3/search/anime', { q: text }))
   if (!res.ok) throw await res.text()
   let json = await res.json()
-  let { title, members, synopsis, episodes, url, rated, score, image_url, type, start_date, end_date, mal_id } = json.results[0]
-  //Scrape Genre MAL by DwiR
-  let res2 = await fetch(`https://myanimelist.net/anime/${mal_id}`)
-  if (!res2.ok) throw await res2.text()
-  let html = await res2.text()
-  let { document } = new JSDOM(html).window
-  let genAnim = [...document.querySelectorAll('div[class="spaceit_pad"] > * a')].map(el => el.href).filter(href => href.startsWith('/anime/genre/'))
-  let animeingfo = `✨️ *Title:* ${title}
-🎆️ *Episodes:* ${episodes}
-🎗️ *Genre:* ${genAnim.join(", ")}
-➡️ *Start date:* ${start_date}
-🔚 *End date:* ${end_date}
-💬 *Show Type:* ${type}
-💌️ *Rating:* ${rated}
+  let { title, synopsis, episodes, url, rated, score, image_url } = json.results[0]
+let animeingfo = ✨ *Title:* ${title}
+🎆 *Episodes:* ${episodes}
+💌 *Rating:* ${rated}
 ❤️ *Score:* ${score}
-👥 *Members:* ${members}
-💚️ *Synopsis:* ${synopsis}
-🌐️ *URL*: ${url}`
+💚 *Synopsis:* ${synopsis}
+🌐 *URL*: ${url}
   conn.sendFile(m.chat, image_url, '', animeingfo, m)
 }
-handler.help = ['anime <judul>']
-handler.tags = ['internet']
+handler.help = ['anime <title>']
+handler.tags = ['anime']
 handler.command = /^(anime|animeinfo)$/i
 //maapin fatur :<
 module.exports = handler
